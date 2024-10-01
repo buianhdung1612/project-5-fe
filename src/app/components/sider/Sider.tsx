d2b2fb7bd55ef6198704c3de4d173b3a884a18e1
buianhdung1612 <buianh09dung@gmail.com>
2024-10-01 17:52:37 +0700

    Project on school

--- src/app/components/sider/Sider.tsx
+++ src/app/components/sider/Sider.tsx
@@ -5,10 +5,11 @@ export default function Sider() {
     return (
       <>
         <div className="bg-[#212121] h-[100vh] fixed w-[280px]">
-            <div className="bg-[#1C1C1C] py-[25px] px-[20px]">
+            <div className="bg-[#1C1C1C] py-[25px] px-[20px] flex justify-centen items-center">
                 <Link href="/">
-                    <img src="/Logo.svg" alt="Logo" className="h-[42px] w-auto"/>
+                    <img src="/logo-2.jpg" alt="Logo" className="h-[60px] w-auto"/>
                 </Link>
+                <div className="text-primary ml-5 text-[30px] font-[900]">Jing mp3</div>
             </div>
             <SiderMenu/>
         </div>        
